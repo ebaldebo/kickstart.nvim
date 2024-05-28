c92fe8fd2e79d8babf5bab72e4e1bd20417ba570
Baldebo, Emil <ebaldebo@gmail.com>
2024-05-28 19:41:44 +0200

    added keymap to toggle Diffview

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -3,5 +3,15 @@ return {
   requires = 'nvim-lua/plenary.nvim',
   config = function()
     require('diffview').setup {}
+    local function toggle_diffview()
+      local view = require('diffview.lib').get_current_view()
+      if view then
+        vim.cmd 'DiffviewClose'
+      else
+        vim.cmd 'DiffviewOpen'
+      end
+    end
+    vim.api.nvim_create_user_command('ToggleDiffview', toggle_diffview, {})
+    vim.api.nvim_set_keymap('n', '<leader>dt', ':ToggleDiffview<CR>', { noremap = true, silent = true })
   end,
 }
